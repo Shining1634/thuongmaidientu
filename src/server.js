5bfbb40a86a366750465f1e4df7ed32f853b9df3
Shining1634 <125346542+Shining1634@users.noreply.github.com>
2026-05-21 11:10:16 +0700

    Refactor server.js for improved organization

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ const allowedOrigins = [
     'http://localhost:3009', // Local development
     'http://localhost:3059', // Local development
     'https://bandodientu-admin.vercel.app',
-    'https://fontend-thuongmaidientuf-admin-5682.vercel.app'
+    'https://fontend-thuongmaidientuf-admin-5682.vercel.app',
     'https://bandodientu-kt-trangchu.vercel.app',
     'https://admin-dodientu.duykhang.site',
     'https://shopbandodientu.duykhang.site',
