30f16070698539663fbde376539cf4ac2e641882
Shining1634 <125346542+Shining1634@users.noreply.github.com>
2026-05-21 10:31:31 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ const allowedOrigins = [
     'http://localhost:3059',
     'https://fontend-thuongmaidientu-zhlt.vercel.app/',
     'https://thuongmaidientu-585e.onrender.com',
-    'https://bandodientu-kt-trangchu.vercel.app',
+    'https://fontend-thuongmaidientu.vercel.app',
     'https://admin-dodientu.duykhang.site',
     'https://shopbandodientu.duykhang.site',
     'https://backend-bandodientu-node.duykhang.site'
